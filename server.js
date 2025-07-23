d8cdacf401e8148ba0d2fc47793230d2948a8759
Lucas Oliveira <luc.osilva@gmail.com>
2025-07-23 13:00:11 -0700

    Define cors origin url

--- server.js
+++ server.js
@@ -12,24 +12,25 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 
-let whitelist = ['https://flight-search-cpsc2650.vercel.app', 'https://flight-search-cpsc2650-git-main-lucasosilvas-projects.vercel.app', 'https://flight-search-cpsc2650-45pxfb839-lucasosilvas-projects.vercel.app'];
+// let whitelist = ['https://flight-search-cpsc2650.vercel.app', 'https://flight-search-cpsc2650-git-main-lucasosilvas-projects.vercel.app', 'https://flight-search-cpsc2650-45pxfb839-lucasosilvas-projects.vercel.app'];
 
-let corsOptions = {
-    origin: function(origin, callback){
-        if(whitelist.indexOf(origin)!==-1){
-            callback(null,true);
-        }else{
-            callback(new Error('Not allowed by cors'));
-        }
-    }
-}
+// let corsOptions = {
+//     origin: function (origin, callback){
+//         if(whitelist.indexOf(origin)!==-1){
+//             callback(null,true);
+//         }else{
+//             callback(new Error('Not allowed by cors'));
+//         }
+//     }
+// }
 
-// app.use(cors({
-//     origin: 'https://flight-search-cpsc2650-i8ssm6w1s-lucasosilvas-projects.vercel.app',
-//     credentials: true
-// }));
+// app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: 'https://flight-search-cpsc2650-45pxfb839-lucasosilvas-projects.vercel.app',
+    credentials: true
+}));
+
 app.use(express.json());
 
 mongoose
